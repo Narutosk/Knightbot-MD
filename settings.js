4deed4ce0fcf2532f8c84711c6718709c8db7acb
Narutosk <sknaruto053@gmail.com>
2025-02-06 01:02:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
-  author: 'Bot',
+  author: 'naruto',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: '𓆩NARUTO AURA𓆪', // Your name
+  ownerNumber: '918389815115', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
